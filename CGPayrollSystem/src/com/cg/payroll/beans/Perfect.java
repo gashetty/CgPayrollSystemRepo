eae4bf18ed2ab229c5e029e13b9fabb1fc20021c
gashetty <gashetty@DIN57000012.corp.capgemini.com>
2019-06-26 16:11:22 +0530

    Intial success

---
+++ CGPayrollSystem/src/com/cg/payroll/beans/Perfect.java
@@ -0,0 +1,14 @@
+package com.cg.payroll.beans;
+
+public class Perfect {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=3,b=4,c;
+		c=a+b;
+		System.out.println("addition is="+c);
+
+	}
+
+}
